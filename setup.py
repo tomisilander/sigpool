69bada82c7ebf79eebb1c82adc6b89510bc1174b
tomisilander <tomi.silander@iki.fi>
2018-12-05 10:50:32 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name          = "sigpool",
+      version       = "0.9",
+      description   = "signal recording class",
+      author        = "Tomi Silander",
+      author_email  = "tomi.silander@iki.fi",
+      py_modules    = ["sigpool"])
